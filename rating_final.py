826e305f8da4e348a5f9ff2a2278665a8b726c18
Mohamad mahdi <m.m.moradi1251@gmail.com>
2024-06-14 15:21:13 +0000

    Upload New File

---
+++ rating_final.py
@@ -0,0 +1,18 @@
+import mysql
+class Rating:
+    def __init__(self,user_id, star_rating, movie_id):
+      
+        self.user_id = user_id
+        self.star_rating = star_rating
+        self.movie_id = movie_id
+    def define_Rating(self,user_id,star_rating,movie_id):
+      query='''insert into rating(user_id,star_rating,movie_id)
+      VALUES (%s, %s, %s)'''
+
+    val=(self.user_id,star_rating)   
+  
+      exec(query,val)
+
+
+      cursor.close()
+      conn.close()
